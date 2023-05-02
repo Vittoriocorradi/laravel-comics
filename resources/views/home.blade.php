3d76e0ad4fd53c702e08045a1105a43f78c44f4f
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-05-02 19:03:11 +0200

    comics list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,19 @@
           <h2>current series</h2>
         </div>
     </section>
-    <div class="big-container">
-        
-    </div>
+    <section class="comics-list d-flex flex-column align-items-center">
+        <div class="big-container d-flex flex-wrap">
+            @foreach ($comics as $comic)
+                <div class="ms-card">
+                    <div class="img">
+                        <img src="{{ $comic['thumb'] }}" alt="">
+                    </div>
+                    <div>{{ $comic['title'] }}</div>
+                </div>
+            @endforeach
+        </div>
+        <div>
+            <button>load more</button>
+        </div>
+    </section>
 @endsection
