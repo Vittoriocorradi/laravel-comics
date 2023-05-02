40c8f24919ce9f544b333710d70a7ccd30f63ffc
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-05-02 21:07:51 +0200

    footer bottom

--- resources/views/partials/footer-bottom.blade.php
+++ resources/views/partials/footer-bottom.blade.php
@@ -1,1 +1,39 @@
-<h1>footer bottom</h1>
+<section class="footer-social">
+    <div class="big-container d-flex align-items-center">
+        <div class="button">
+            <button>
+                sign-up now!
+            </button>
+        </div>
+        <div class="icons-group d-flex align-items-center">
+            <div class="follow-us">
+                follow us
+            </div>
+            <div class="icon">
+                <a href="#">
+                    <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="icon.alt">
+                </a>
+            </div>
+            <div class="icon">
+                <a href="#">
+                    <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="icon.alt">
+                </a>
+            </div>
+            <div class="icon">
+                <a href="#">
+                    <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="icon.alt">
+                </a>
+            </div>
+            <div class="icon">
+                <a href="#">
+                    <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="icon.alt">
+                </a>
+            </div>
+            <div class="icon">
+                <a href="#">
+                    <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="icon.alt">
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
